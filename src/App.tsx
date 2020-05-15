bf54e6d0830d07dfc561158b86be6317d3af7f52
Theodore Anderson <theoanderson3@gmail.com>
2020-05-15 10:37:40 -0700

    add in local storage method

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { Todo } from './Todo';
 import { TodoForm } from './TodoForm';
 import { ITodo } from './interfaces';
 
+const saveArrayToLocalStorage = (key: string, arr: any[] | undefined) => {
+  if(!arr?.length) return;
+  window.localStorage.setItem(key, JSON.stringify(arr))
+}
+
+const retrieveFromLocalStorage = (key: string) => window.localStorage.getItem(key)
+
 
 function App() {
 
   const [todos, setTodos ] =  useState<ITodo[] | undefined>([])
+
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    if(!isLoading) {
+      const retrievedTodos = retrieveFromLocalStorage('todos')
+      if(retrievedTodos !== null) {
+        setTodos(JSON.parse(retrievedTodos))
+      }
+      setIsLoading(true)
+    }
+    return () => saveArrayToLocalStorage('todos', todos)
+  }, [isLoading, todos])
 
   const onRemoveTodo = (id: number) => setTodos(todos?.filter(t => t.id !== id))
 
@@ -30,8 +50,6 @@ function App() {
   const onSubmit = (todo: ITodo) => {
     setTodos(todos?.concat({ ...todo, created_at: new Date( ), id: todos.length ? todos.length + 1 : 1}))
   }
-
-  console.log(todos);
 
   return (
     <div className="App">
